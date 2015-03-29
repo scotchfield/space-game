0f9c378efba6720f9ca350fd89e73ec32c7cce7e
dragonpaulz <paul.geesaman@gmail.com>
2015-03-29 12:38:17 -0400

    Forgot to add a file from last commit

---
+++ space-game/space-game/Enumerations/TurnActions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_game.Enumerations
+{
+    enum TurnActions
+    {
+        Gain2Energy,
+        Gain1Energy1Card,
+        PlayCard
+    }
+}
